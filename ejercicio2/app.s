372c57b90549704a29bb4c886d136d88564b551a
Fransuaa <fran.lopezbenavides@gmail.com>
2025-06-05 12:17:35 -0300

    modifico app.s (ahora tiene el main del ej1)

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,80 +1,110 @@
-.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL,  	32
-
+	.include "constants.s"
 	.globl main
 
+
+/*  MAIN DE EJEMPLO ANIMACION (GONZA)
+	main:
+		// x0 contiene la dirección base del framebuffer
+		mov x20, x0		// Guarda la dirección base del framebuffer en x20
+		mov x28, x20	// Configura x28 para las funciones de utils.s
+
+		// Definir colores
+		movz w25, 0xFF00, lsl 16  
+		movk w25, 0x0000, lsl 0
+		movz w26, 0xFFFF, lsl 16  
+		movk w26, 0xFFFF, lsl 0
+
+		// Definir posiciones iniciales
+		mov x22, #200  // x del primer circulo
+		mov x23, #220  // x del segundo circulo
+		mov x24, #200  // x del tercer circulo
+		mov x21, #400  // y inicial del primer circulo
+		mov x27, #435  // y inicial del segundo circulo
+		mov x19, #470  // y inicial del tercer circulo
+
+	animation_loop:
+		// Pintar el fondo 
+		mov w0, w25   // color 
+		mov x1, #0    // x_start
+		mov x2, #0    // y_start
+		mov x3, #639  // x_end
+		mov x4, #479  // y_end
+		bl draw_rectangle
+
+		// Dibujar el primer circulo
+		mov w0, w26   
+		mov x3, x22   
+		mov x4, x21   
+		mov x5, #10   
+		bl draw_fill_circle
+
+		// Dibujar el segundo circulo
+		mov w0, w26
+		mov x3, x23
+		mov x4, x27   
+		mov x5, #10
+		bl draw_fill_circle
+
+		// Dibujar el tercer circulo
+		mov w0, w26
+		mov x3, x24
+		mov x4, x19   
+		mov x5, #10
+		bl draw_fill_circle
+
+		// Actualizar la posición y de cada circulo
+		sub x21, x21, #2  // Mover primer circulo hacia arriba
+		cmp x21, #0
+		b.ge no_reset1
+		mov x21, #470     // Reiniciar y del primer circulo
+	no_reset1:
+
+		sub x27, x27, #2  // Mover segundo circulo hacia arriba
+		cmp x27, #0
+		b.ge no_reset2
+		mov x27, #420     // Reiniciar y del segundo circulo
+	no_reset2:
+
+		sub x19, x19, #2  // Mover tercer circulo hacia arriba
+		cmp x19, #0
+		b.ge no_reset3
+		mov x19, #370     // Reiniciar y del tercer circulo
+	no_reset3:
+
+		// Retardo para controlar la velocidad
+		movz x8, #0x98, lsl 16   // Valor para el delay (10,000,000)
+		movk x8, #0x9680, lsl 0
+		bl delay
+
+		b animation_loop  // Repetir la animacion
+*/
+
+
 main:
-	// x0 contiene la dirección base del framebuffer
-	mov x20, x0		// Guarda la dirección base del framebuffer en x20
-	mov x28, x20	// Configura x28 para las funciones de utils.s
+	// x0 contiene la direccion base del framebuffer
+ 	mov x28, x0	// Guarda la dirección base del framebuffer en x28
+	//---------------- CODE HERE ------------------------------------
+	movz x0, 0x40, lsl #16
+    movk x0, 0xD0CF, lsl #0
+	movz x1, 0x17, lsl #16
+    movk x1, 0x6CD6, lsl #0
+	bl background_gradient
+	bl draw_flower
 
-	// Definir colores
-	movz w25, 0xFF00, lsl 16  
-	movk w25, 0x0000, lsl 0
-	movz w26, 0xFFFF, lsl 16  
-	movk w26, 0xFFFF, lsl 0
+	bl draw_sand
+	bl draw_road
+	bl draw_odc_sign
+	bl draw_edificios_fondo
 
-	// Definir posiciones iniciales
-	mov x22, #200  // x del primer circulo
-	mov x23, #220  // x del segundo circulo
-	mov x24, #200  // x del tercer circulo
-	mov x21, #400  // y inicial del primer circulo
-	mov x27, #435  // y inicial del segundo circulo
-	mov x19, #470  // y inicial del tercer circulo
 
-animation_loop:
-	// Pintar el fondo 
-	mov w0, w25   // color 
-	mov x1, #0    // x_start
-	mov x2, #0    // y_start
-	mov x3, #639  // x_end
-	mov x4, #479  // y_end
-	bl draw_rectangle
+	bl draw_casa_calamardo
+	bl draw_casa_patricio
+	bl draw_casa_bob
 
-	// Dibujar el primer circulo
-	mov w0, w26   
-	mov x3, x22   
-	mov x4, x21   
-	mov x5, #10   
-	bl draw_fill_circle
+	
 
-	// Dibujar el segundo circulo
-	mov w0, w26
-	mov x3, x23
-	mov x4, x27   
-	mov x5, #10
-	bl draw_fill_circle
+	//---------------------------------------------------------------
+	// Infinite Loopgi
 
-	// Dibujar el tercer circulo
-	mov w0, w26
-	mov x3, x24
-	mov x4, x19   
-	mov x5, #10
-	bl draw_fill_circle
-
-	// Actualizar la posición y de cada circulo
-	sub x21, x21, #2  // Mover primer circulo hacia arriba
-	cmp x21, #0
-	b.ge no_reset1
-	mov x21, #470     // Reiniciar y del primer circulo
-no_reset1:
-
-	sub x27, x27, #2  // Mover segundo circulo hacia arriba
-	cmp x27, #0
-	b.ge no_reset2
-	mov x27, #420     // Reiniciar y del segundo circulo
-no_reset2:
-
-	sub x19, x19, #2  // Mover tercer circulo hacia arriba
-	cmp x19, #0
-	b.ge no_reset3
-	mov x19, #370     // Reiniciar y del tercer circulo
-no_reset3:
-
-	// Retardo para controlar la velocidad
-	movz x8, #0x98, lsl 16   // Valor para el delay (10,000,000)
-	movk x8, #0x9680, lsl 0
-	bl delay
-
-	b animation_loop  // Repetir la animacion
+InfLoop:
+	b InfLoop
